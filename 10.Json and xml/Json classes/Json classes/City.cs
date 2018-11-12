0162c813021ffe31f756fb4d3b2e95a573b084e3
nekorytaylor666 <art_gulba@mail.ru>
2018-11-12 20:07:27 +0600

    Xml homework

---
+++ 10.Json and xml/Json classes/Json classes/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json_classes
+{
+    class City
+    {
+        public string Name { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+    }
+}
